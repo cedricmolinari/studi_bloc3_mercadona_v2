8b3afa3180ccb74ed76cbf01354182cc8bf04705
cedricm <cedric.molinari0@gmail.com>
2023-09-04 23:01:36 +0200

    first commit

---
+++ src/test/java/com/mercadona/promotionmanagement/PromotionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadona.promotionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
